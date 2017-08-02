e61fdcadd015c74a0a2962572530d1467f057220
刘毅 <liuyi@quanquan6.com>
2017-08-02 16:06:00 +0800

    add code comments

--- pageSource/pageSource/LYWebView.m
+++ pageSource/pageSource/LYWebView.m
@@ -9,39 +9,27 @@
 #import "LYWebView.h"
 
 @interface LYWebView()
-
+//显示网页来源的label
 @property (nonatomic, strong) UILabel *sourceLabel;
+//存储URL中host部分
 @property (nonatomic, strong) NSString *source;
+
 @end
 
 @implementation LYWebView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
-//- (instancetype)init
-//{
-//    self = [super init];
-//    if (self)
-//    {
-//        [self sourceLabel];
-//    }
-//    return self;
-//}
-
+//重写webview的loadRequest方法，将网页来源的相关设置写在这里，这样使用时可以默认具有此特性，无需额外设置
 - (void)loadRequest:(NSURLRequest *)request
 {
     [super loadRequest:request];
     self.source = request.URL.host;
+//    显示网页来源的UI更新
     dispatch_async(dispatch_get_main_queue(), ^{
         [self sourceLabel];
     });
 }
 
+//显示网页来源label的懒加载get方法
 - (UILabel *)sourceLabel
 {
     if(!_sourceLabel)
@@ -50,6 +38,7 @@
         source.textColor = [UIColor whiteColor];
         source.textAlignment = NSTextAlignmentCenter;
         source.font = [UIFont systemFontOfSize:12.0];
+//        将label嵌入到scrollview下面
         [self insertSubview:source belowSubview:self.scrollView];
         source.text = [NSString stringWithFormat:@"此网页由 %@ 提供", self.source];
         return source;
@@ -60,10 +49,4 @@
     }
 }
 
-//- (void)webViewDidFinishLoad:(UIWebView *)webView
-//{
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        [self sourceLabel];
-//    });
-//}
 @end
--- pageSource/pageSource/ViewController.m
+++ pageSource/pageSource/ViewController.m
@@ -17,16 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    LYWebView *webview = [[LYWebView alloc] initWithFrame:CGRectMake(0, 40, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - 40)];
+    LYWebView *webview = [[LYWebView alloc] initWithFrame:CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - 40)];
     [self.view addSubview:webview];
     [webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://portal.3g.qq.com/?aid=index&g_ut=3"]]];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
